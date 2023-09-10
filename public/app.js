3b5124ac3e7ebc15ff3c38fb9c7dbb14c4d156c0
Iamneo <techadmin@iamneo.ai>
2023-09-10 18:06:55 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -15,75 +15,74 @@ const resetButton = document.querySelector('#reset-button');
 
 // Add click event listeners to each button
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-            // Check if the button is empty and the game is not over
-                    if (button.value === '' && !isGameOver) {
-                                button.value = currentPlayer;
-                                            button.disabled = true;
+button.addEventListener('click', () => {
+// Check if the button is empty and the game is not over
+if (button.value === '' && !isGameOver) {
+button.value = currentPlayer;
+button.disabled = true;
 
-                                                        // Check for a win or draw
-                                                                    if (checkWin() || checkDraw()) {
-                                                                                    isGameOver = true;
-                                                                                                    resultContainer.textContent = isGameOver ? 'Game Over' : '';
+// Check for a win or draw
+if (checkWin() || checkDraw()) {
+isGameOver = true;
+resultContainer.textContent = isGameOver ? 'Player Won' : '';
 
-                                                                                                                    // Enable the reset button
-                                                                                                                                    resetButton.disabled = false;
-                                                                                                                                                } else {
-                                                                                                                                                                // Switch to the other player's turn
-                                                                                                                                                                                currentPlayer = currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
-                                                                                                                                                                                                resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                            }
-                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                        });
-                                                                                                                                                                                                                        });
+// Enable the reset button
+resetButton.disabled = false;
+} else {
+// Switch to the other player's turn
+currentPlayer = currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
+resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
+}
+}
+});
+});
 
-                                                                                                                                                                                                                        // Reset the game when the reset button is clicked
-                                                                                                                                                                                                                        resetButton.addEventListener('click', () => {
-                                                                                                                                                                                                                            buttons.forEach(button => {
-                                                                                                                                                                                                                                    button.value = '';
-                                                                                                                                                                                                                                            button.disabled = false;
-                                                                                                                                                                                                                                                });
+// Reset the game when the reset button is clicked
+resetButton.addEventListener('click', () => {
+buttons.forEach(button => {
+button.value = '';
+button.disabled = false;
+});
 
-                                                                                                                                                                                                                                                    // Reset game state
-                                                                                                                                                                                                                                                        currentPlayer = PLAYER_X;
-                                                                                                                                                                                                                                                            isGameOver = false;
-                                                                                                                                                                                                                                                                resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                                                                                    resetButton.disabled = true;
-                                                                                                                                                                                                                                                                    });
+// Reset game state
+currentPlayer = PLAYER_X;
+isGameOver = false;
+resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
+resetButton.disabled = true;
+});
 
-                                                                                                                                                                                                                                                                    // Function to check if a player has won
-                                                                                                                                                                                                                                                                    function checkWin() {
-                                                                                                                                                                                                                                                                        // Define winning combinations
-                                                                                                                                                                                                                                                                            const winCombos = [
-                                                                                                                                                                                                                                                                                    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-                                                                                                                                                                                                                                                                                            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-                                                                                                                                                                                                                                                                                                    [0, 4, 8], [2, 4, 6] // Diagonals
-                                                                                                                                                                                                                                                                                                        ];
+// Function to check if a player has won
+function checkWin() {
+// Define winning combinations
+const winCombos = [
+[0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+[0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+[0, 4, 8], [2, 4, 6] // Diagonals
+];
 
-                                                                                                                                                                                                                                                                                                            // Check if any winning combination is present
-                                                                                                                                                                                                                                                                                                                for (const combo of winCombos) {
-                                                                                                                                                                                                                                                                                                                        const [a, b, c] = combo;
-                                                                                                                                                                                                                                                                                                                                if (buttons[a].value && buttons[a].value === buttons[b].value && buttons[b].value === buttons[c].value) {
-                                                                                                                                                                                                                                                                                                                                            resultContainer.textContent = `Player ${currentPlayer} wins!`;
-                                                                                                                                                                                                                                                                                                                                                        return true;
-                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                    }
+// Check if any winning combination is present
+for (const combo of winCombos) {
+const [a, b, c] = combo;
+if (buttons[a].value && buttons[a].value === buttons[b].value && buttons[b].value === buttons[c].value) {
+resultContainer.textContent = `Player ${currentPlayer} wins!`;
+return true;
+}
+}
 
-                                                                                                                                                                                                                                                                                                                                                                        return false;
-                                                                                                                                                                                                                                                                                                                                                                        }
+return false;
+}
 
-                                                                                                                                                                                                                                                                                                                                                                        // Function to check if the game is a draw
-                                                                                                                                                                                                                                                                                                                                                                        function checkDraw() {
-                                                                                                                                                                                                                                                                                                                                                                            // If all buttons are filled and no player has won, it's a draw
-                                                                                                                                                                                                                                                                                                                                                                                const isDraw = Array.from(buttons).every(button => button.value !== '');
-                                                                                                                                                                                                                                                                                                                                                                                    if (isDraw) {
-                                                                                                                                                                                                                                                                                                                                                                                            resultContainer.textContent = "It's a draw!";
-                                                                                                                                                                                                                                                                                                                                                                                                    return true;
-                                                                                                                                                                                                                                                                                                                                                                                                        }
+// Function to check if the game is a draw
+function checkDraw() {
+// If all buttons are filled and no player has won, it's a draw
+const isDraw = Array.from(buttons).every(button => button.value !== '');
+if (isDraw) {
+resultContainer.textContent = "It's a draw!";
+return true;
+}
 
-                                                                                                                                                                                                                                                                                                                                                                                                            return false;
-                                                                                                                                                                                                                                                                                                                                                                                                            }
+return false;
+}
 
-                                                                                                                                                                                                                                                                                                                                                                                                            // Initialize the game with Player X's turn
-                                                                                                                                                                                                                                                                                                                                                                                                            resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                                                                                                                                                                                                                            
+// Initialize the game with Player X's turn
+resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
